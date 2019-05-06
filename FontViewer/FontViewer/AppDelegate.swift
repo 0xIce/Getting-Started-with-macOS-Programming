15a8ea5974414c704eac0a02bab7dfea40eb0499
hongqima <hongqima@yangqianguan.com>
2019-05-06 14:41:00 +0800

    init project

---
+++ FontViewer/FontViewer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FontViewer
+//
+//  Created by 马红奇 on 2019/5/6.
+//  Copyright © 2019 hongqima. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ FontViewer/FontViewer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FontViewer
+//
+//  Created by 马红奇 on 2019/5/6.
+//  Copyright © 2019 hongqima. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
